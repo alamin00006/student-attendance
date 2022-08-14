79fa1f8d0f370321801fe30eff70653b83a99667
Mohammad Al Amin <alaminbamna08@gmail.com>
2022-08-14 11:30:11 +0600

    navbar and student login, teacher login form created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navber from './components/Navber/Navber';
+import Student from './components/Student/Student';
+import Teacher from './components/Teacher/Teacher';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+       <Navber></Navber>
+       <Routes>
+       <Route  path='/student' element={<Student></Student>}></Route>
+       <Route  path='/teacher' element={<Teacher></Teacher>}></Route>
+       </Routes>
+      
     </div>
   );
 }
---
+++ src/components/Navber/Navber.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import '../Navber/Navber.css'
+import { Link, NavLink } from 'react-router-dom';
+
+const Navber = () => {
+    return (
+        <div>
+          <Navbar>
+      <Container>
+      <Link to="/" class="btn btn-ghost  text-2xl uppercase">Student Managment</Link>
+        
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+      <NavLink to ='/student'>Student Sing in</NavLink>
+      <NavLink to ='/teacher'>Teacher Sing in</NavLink>
+       
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>  
+        </div>
+    );
+};
+
+export default Navber;
